1881b0016e8634a3bbe3082d88b191f75cc99564
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-10-29 01:00:47 +0900

    move to src

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,12 @@ function errorHandler(err) {
   console.log('Error: ' + err.message);
 }
 
+var paths = {
+  statics: 'src/**/*.{html,json,png}',
+  css: 'src/**/*.scss',
+  build: './build'
+};
+
 gulp.task('build:js', function() {
   var bundler = browserify({entries: ['src/app.js']});
 
@@ -30,21 +36,27 @@ gulp.task('build:js', function() {
       .on('error', errorHandler)
       .pipe(source('app.js'))
       .pipe(buffer())
-      .pipe(gulp.dest('./dist/'));
+      .pipe(gulp.dest(paths.build));
   }
 
   return b();
 });
 
 gulp.task('build:css', function() {
-  gulp.src('src/*.scss')
+  gulp.src(paths.css)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/'));
-
-  if (watching) {
-    gulp.watch('src/*.scss', ['build:css']);
-  }
+    .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('build', ['build:css', 'build:js']);
+gulp.task('build:copy', function() {
+  gulp.src(paths.statics)
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('build', ['build:copy', 'build:css', 'build:js'], function() {
+  if (watching) {
+    gulp.watch(paths.statics, ['build:copy']);
+    gulp.watch(paths.css, ['build:css']);
+  }
+});
 gulp.task('watch', ['enable-watch', 'build']);
